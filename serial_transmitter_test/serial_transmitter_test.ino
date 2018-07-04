2a1fc9f8359d1186428fa5e464986c3ac4f549dd
Kuan Hou Lee <angus.lee809@gmail.com>
2018-07-04 15:21:25 +1000

    Removed parity bits from teensy

--- serial_transmitter_test/serial_transmitter_test.ino
+++ serial_transmitter_test/serial_transmitter_test.ino
@@ -1,5 +1,5 @@
 int pin_LED = 13;
-# define HWSERIAL Serial1
+#define HWSERIAL Serial1
 
 void setup() {
   pinMode(pin_LED, OUTPUT);
@@ -9,19 +9,19 @@ void setup() {
   // See here: https://www.pjrc.com/teensy/td_uart.html
   int baud_rate = 500000;
   Serial.begin(baud_rate);    // USB Serial 
-  HWSERIAL.begin(baud_rate, SERIAL_8O1/*8 Data bits, Odd Parity*/);  // Hardware Serial (RX & TX pins)    
+  HWSERIAL.begin(baud_rate, SERIAL_8N1/*8 Data bits, No Parity bits*/);  // Hardware Serial (RX & TX pins)    
 }
 
 void loop() {
   // LED ON
   digitalWrite(pin_LED, HIGH);
   Serial.println("LED ON");
-  HWSERIAL.write("LED ON");
+  HWSERIAL.write("ON");
   delay(500);
   
   // LED OFF
   digitalWrite(pin_LED, LOW);
   Serial.println("LED OFF");
-  HWSERIAL.write("LED OFF");
+  HWSERIAL.write("OFF");
   delay(500);
 }
