9dc8265a3ff86b70e9cf1d5cda0bbef89a28a17e
Kuan Hou Lee <angus.lee809@gmail.com>
2018-07-04 13:40:08 +1000

    Add the teensy sketch

--- serial_transmitter_test/serial_transmitter_test.ino
+++ serial_transmitter_test/serial_transmitter_test.ino
@@ -1,12 +1,27 @@
 int pin_LED = 13;
+# define HWSERIAL Serial1
 
 void setup() {
-  pinMode(pin_LED, OUTPUT);     
+  pinMode(pin_LED, OUTPUT);
+  
+  // Set baud rate to 500000 
+  // Fastest speed for Teensy LC before errors occuring
+  // See here: https://www.pjrc.com/teensy/td_uart.html
+  int baud_rate = 500000;
+  Serial.begin(baud_rate);    // USB Serial 
+  HWSERIAL.begin(baud_rate);  // Hardware Serial (RX & TX pins)    
 }
 
 void loop() {
-  digitalWrite(pin_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);               // wait for a second
-  digitalWrite(pin_LED, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);               // wait for a second
+  // LED ON
+  digitalWrite(pin_LED, HIGH);
+  delay(500);
+  Serial.write("LED ON");
+  HWSERIAL.write("LED ON");
+  
+  // LED OFF
+  digitalWrite(pin_LED, LOW);
+  delay(500);
+  Serial.write("LED OFF");
+  HWSERIAL.write("LED OFF");
 }
